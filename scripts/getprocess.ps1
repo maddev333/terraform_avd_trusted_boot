9b96d97998e7c8c904eda2b5bb312cf7e07fcc74
maddev333 <maddev333@gmail.com>
2023-10-20 08:22:02 -0400

    updates

--- scripts/getprocess.ps1
+++ scripts/getprocess.ps1
@@ -1,1 +1,5 @@
 Get-Process | Out-File -FilePath c:\\Process.txt
+New-Item -Path 'HKLM:\Software\FSLogix' -Force | New-ItemProperty -Name 'Profiles' -Type DWord -Value 1 -Force 
+Get-ItemProperty HKLM:\Software\FSLogix -Name Profiles
+
+
